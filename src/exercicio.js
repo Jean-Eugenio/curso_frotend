acef5d7bcae78b7f161b2d0cadff09705fe13b9b
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-10-19 13:43:15 -0300

    Add files via upload

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function calculeArea(base, altura) {
+    return base * altura;
+}
+function dizOla(nome) {
+    return "Olá " + nome;
+}
+console.log(calculeArea(10, 20));
+console.log(dizOla("Matheus"));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function calculeArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+
+function dizOla(nome: string): string {
+    return "Olá " + nome;
+}
+
+console.log(calculeArea(10,20));
+console.log(dizOla("Matheus")); 
