c15f8131be0bb794ae0418b1ba4566c2bf8127bf
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-11-04 14:00:08 -0300

    Add files via upload

---
+++ cypress/e2e/agenda-contatos/agenda_teste.cy.js
@@ -0,0 +1,45 @@
+/// <reference types="cypress" />
+
+describe('Testes de Crud Contatos', () => {
+    
+    beforeEach(()=>{
+        cy.visit('https://agenda-contatos-react.vercel.app/')
+    })
+
+    it('deve testar o montagem da App', () => {
+        cy.get('header > h1').should('have.length.greaterThan', 0)
+        cy.get('form > input').should('have.length', 3)
+    })
+
+    it('deve incluir um novo contato', () => {
+        cy.get('[type="text"]').type('Jean eugenio')
+        cy.get('[type="email"]').type('Jeaneugenioteste@gmail.com')
+        cy.get('[type="tel"]').type('21 12345678')
+        cy.get('.adicionar').click()
+    })  
+    
+    it('deve ativar modo de edição', () => {
+        cy.get(':nth-child(2) > .sc-gueYoa > .edit').click()
+        cy.get('[type="text"]').should('have.length.greaterThan', 0)
+    })
+
+    it('deve alterar o primeiro contato selecionado', () => {
+        cy.get('.edit').first().click()
+
+        cy.get('[type="text"]').clear()
+        cy.get('[type="text"]').type('Gabriel barbosa')
+
+        cy.get('[type="email"]').clear()
+        cy.get('[type="email"]').type('gabrielbarbosa@Gmail.com')
+
+        cy.get('[type="tel"]').clear()
+        cy.get('[type="tel"]').type('21 19202124')
+
+        cy.get('.alterar').click()
+    })
+
+    it('Deve apagar o ultimo contato', () => {
+        
+        cy.get('.delete').last().click()
+    })
+})
